89d18fb714dfab8d097e2e9647c742e21a1cc1f4
Gavin Gregory Camlin <35441754+thetamer11@users.noreply.github.com>
2025-11-28 07:57:31 -0800

    init push with fixed flac

--- src/register_types.cpp
+++ src/register_types.cpp
@@ -1,38 +1,49 @@
 #include "register_types.h"
 
-#include <gdextension_interface.h>
-#include <godot_cpp/core/class_db.hpp>
-#include <godot_cpp/core/defs.hpp>
-#include <godot_cpp/godot.hpp>
+#include "audio_stream_flac.h"
+#include "resource_format_loader_flac.h"
 
-#include "example_class.h"
+#include <gdextension_interface.h>
+#include <godot_cpp/core/defs.hpp>
+#include <godot_cpp/core/class_db.hpp>
+#include <godot_cpp/godot.hpp>
+#include <godot_cpp/classes/resource_loader.hpp>
 
 using namespace godot;
 
-void initialize_gdextension_types(ModuleInitializationLevel p_level)
-{
-	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
-		return;
-	}
-	GDREGISTER_CLASS(ExampleClass);
+static Ref<ResourceFormatLoaderFLAC> resource_loader_flac;
+
+void initialize_flac_module(ModuleInitializationLevel p_level) {
+    if (p_level != MODULE_INITIALIZATION_LEVEL_EDITOR) {
+        return;
+    }
+
+    // TODO: implement resourceFormatSaver/ ResourceFormatLoader, otherwise importer will not work
+    ClassDB::register_class<AudioStreamFLAC>();
+    ClassDB::register_class<AudioStreamPlaybackFLAC>();
+    ClassDB::register_class<ResourceFormatLoaderFLAC>();
+    resource_loader_flac.instantiate();
+    ResourceLoader::get_singleton()->add_resource_format_loader(resource_loader_flac);
 }
 
-void uninitialize_gdextension_types(ModuleInitializationLevel p_level) {
-	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
-		return;
-	}
+void uninitialize_flac_module(ModuleInitializationLevel p_level) {
+    if (p_level != MODULE_INITIALIZATION_LEVEL_EDITOR) {
+        return;
+    }
+
+    ResourceLoader::get_singleton()->remove_resource_format_loader(resource_loader_flac);
+    resource_loader_flac.unref();
 }
 
-extern "C"
-{
-	// Initialization
-	GDExtensionBool GDE_EXPORT example_library_init(GDExtensionInterfaceGetProcAddress p_get_proc_address, GDExtensionClassLibraryPtr p_library, GDExtensionInitialization *r_initialization)
-	{
-		GDExtensionBinding::InitObject init_obj(p_get_proc_address, p_library, r_initialization);
-		init_obj.register_initializer(initialize_gdextension_types);
-		init_obj.register_terminator(uninitialize_gdextension_types);
-		init_obj.set_minimum_library_initialization_level(MODULE_INITIALIZATION_LEVEL_SCENE);
+extern "C" {
+// Initialization.
+GDExtensionBool GDE_EXPORT flac_library_init(GDExtensionInterfaceGetProcAddress p_get_proc_address, const GDExtensionClassLibraryPtr p_library, GDExtensionInitialization *r_initialization) {
+    godot::GDExtensionBinding::InitObject init_obj(p_get_proc_address, p_library, r_initialization);
 
-		return init_obj.init();
-	}
+    init_obj.register_initializer(initialize_flac_module);
+    init_obj.register_terminator(uninitialize_flac_module);
+    init_obj.set_minimum_library_initialization_level(MODULE_INITIALIZATION_LEVEL_EDITOR);
+
+    return init_obj.init();
+}
 }
--- src/register_types.h
+++ src/register_types.h
@@ -1,7 +1,7 @@
-#ifndef EXAMPLE_REGISTER_TYPES_H
-#define EXAMPLE_REGISTER_TYPES_H
+#ifndef GD_FLAC_MODULE_REGISTER_TYPES_H
+#define GD_FLAC_MODULE_REGISTER_TYPES_H
 
-void initialize_gdextension_types();
-void uninitialize_gdextension_types();
+void initialize_flac_module();
+void uninitialize_flac_module();
 
-#endif // EXAMPLE_REGISTER_TYPES_H
+#endif // GD_FLAC_EXTENSION_REGISTER_TYPES_H
